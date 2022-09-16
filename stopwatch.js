240837f0a9222cbf0e9548f1e9e141e9828c8d5a
Abdurrahman Mohamoud <abdurrahman.mohamoud@weareadaptive.com>
2022-09-16 11:16:02 +0100

    Start Stop and Reset buttons now working

--- stopwatch.js
+++ stopwatch.js
@@ -1,10 +1,14 @@
-const time = document.getElementById("timer")
-
+const time = document.getElementById("timer"); 
+const startButton = document.getElementById('start'); 
+const resetButton = document.getElementById('reset');
 
 
 let milliseconds = 0; 
 let seconds = 0; 
 let minutes = 0; 
+let currentTime = 0;
+let watchStatus = "off"; 
+
 
 
 //increment function 
@@ -22,21 +26,63 @@ function stopwatch () {
         }
     } 
 
-    time.innerHTML = "0" + minutes + ":" + "0" + seconds + ":"  + milliseconds;
+   time.innerHTML = "0" + minutes + ":" + "0" + seconds + ":"  + milliseconds;
 }
 
- window.setInterval(stopwatch, 10); 
+function calculateLap () {
+    console.log("lap Calculated")
+}
+
+
+
 
  //execute function when start button is pressed. 
+ function startTime() {
+    if (watchStatus === "off") { 
+       myInterval = window.setInterval(stopwatch, 10);
+        watchStatus = "on"; 
 
+        resetButton.innerHTML = "Lap"; 
+        startButton.innerHTML = "Stop"
+     } else if (watchStatus === "on") {
+
+        window.clearInterval(stopwatch);
+        watchStatus = "off"; 
+        resetButton.innerHTML = "Reset"; 
+        startButton.innerHTML = "Start"  
+        clearInterval(myInterval)
+    }
+ } 
+
+ document.getElementById("start").addEventListener("click", startTime);
  
- //when start button is pressed, change the value of start to stop
-
 
  //when reset is pressed, reset the timer to the default value 
+function resetTimeLapToggle() {
+    if (resetButton.innerHTML === "Reset") { 
+        milliseconds = 0; 
+        seconds  = 0; 
+        minutes = 0
+        watchStatus = "off"
+        clearInterval(myInterval) 
+        time.innerHTML = "0" + minutes + ":" + "0" + seconds + ":"  + "0" + milliseconds;
+        console.log("pressed reset")
+        
+    } else {
+        console.log("calculated Lap")
+    }
+       
+    
+}
+resetButton.addEventListener('click', resetTimeLapToggle)
 
 
- //when reset is pressed, change value of reset to lap 
+
+//if lap is pressed, execute lap calculation
+
+
+
+
 
 
 

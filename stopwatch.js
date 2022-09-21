aeddc366150ff62e017db604409ec08a85fa5352
Abdurrahman Mohamoud <abdurrahman.mohamoud@weareadaptive.com>
2022-09-21 13:34:53 +0100

    formatted time from milliseconds to stopwatch time

--- stopwatch.js
+++ stopwatch.js
@@ -4,32 +4,22 @@ const resetButton = document.getElementById('reset');
 
 
 let milliseconds = 0; 
-let seconds = 0; 
-let minutes = 0; 
-let currentTime = 0;
 let watchStatus = "off"; 
 
 
 
 //increment function 
 
-function stopwatch () { 
-    milliseconds++; 
+const stopwatch = () => {  
+ time.innerHTML =  formatTime(milliseconds);
+// console.log(milliseconds)
+; 
+milliseconds++;
+    
 
-    if (milliseconds / 100 === 1) {
-        seconds++; 
-        milliseconds =0;
-
-        if (seconds / 60 === 1) {
-            minutes ++; 
-            seconds = 0;
-        }
-    } 
-
-   time.innerHTML = "0" + minutes + ":" + "0" + seconds + ":"  + milliseconds;
 }
 
-function calculateLap () {
+const calculateLap = () => {
     console.log("lap Calculated")
 }
 
@@ -37,16 +27,16 @@ function calculateLap () {
 
 
  //execute function when start button is pressed. 
- function startTime() {
-    if (watchStatus === "off") { 
-       myInterval = window.setInterval(stopwatch, 10);
+ const startTime = () => {
+     if (watchStatus === "off") { 
+        myInterval = window.setInterval(stopwatch, 10);
         watchStatus = "on"; 
-
+        
         resetButton.innerHTML = "Lap"; 
         startButton.innerHTML = "Stop"
      } else if (watchStatus === "on") {
 
-        window.clearInterval(stopwatch);
+        //window.clearInterval(stopwatch);
         watchStatus = "off"; 
         resetButton.innerHTML = "Reset"; 
         startButton.innerHTML = "Start"  
@@ -58,14 +48,13 @@ function calculateLap () {
  
 
  //when reset is pressed, reset the timer to the default value 
-function resetTimeLapToggle() {
+const resetTimeLapToggle = () => {
     if (resetButton.innerHTML === "Reset") { 
         milliseconds = 0; 
-        seconds  = 0; 
-        minutes = 0
+    
         watchStatus = "off"
         clearInterval(myInterval) 
-        time.innerHTML = "0" + minutes + ":" + "0" + seconds + ":"  + "0" + milliseconds;
+        time.innerHTML = milliseconds //use backticks
         console.log("pressed reset")
         
     } else {
@@ -75,11 +64,37 @@ function resetTimeLapToggle() {
     
 }
 resetButton.addEventListener('click', resetTimeLapToggle)
+''
+
+const formatTime = (time) =>{ 
+
+    let formattedMilliseconds = 0;
+    let formattedSeconds = 0; 
+    let formattedMinutes = 0; 
+    
+        
+
+    formattedMilliseconds = time % 100; 
+    formattedSeconds = Math.floor(time/100);
+    formattedMinutes= Math.floor((formattedSeconds/60)) 
+
+
+    formattedSeconds = formattedSeconds % 60; 
+    formattedMinutes = formattedMinutes % 60;
+
+   
+
+
+    return `${formattedMinutes} : ${ formattedSeconds} : ${formattedMilliseconds}`
+
+  
+    
+    } 
+
 
 
 
 //if lap is pressed, execute lap calculation
-
 
 
 
